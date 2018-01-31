1178ab365bad8e0ca6fb7f48d4e620076f960b4a
Cody Short <nccgs117@gmail.com>
2018-01-31 17:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine,customer) {
   katzDeliLine.push(customer)
-  return `Welcome, ${customer}. You are number ` + (katzDeliLine.length - 1) + ` in line.`
+  return `Welcome, ${customer}. You are number ` + (katzDeliLine.length) + ` in line.`
 }
 
 function nowServing(){
