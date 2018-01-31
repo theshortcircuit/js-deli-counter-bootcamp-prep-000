2fc66d7ff74be023e62321f21b46544f0fa0ab69
Cody Short <nccgs117@gmail.com>
2018-01-31 17:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function takeANumber(katzDeliLine,customer) {
 }
 
 function nowServing(){
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine[0]
+  if (katzDeli.length > 0) {
+    return "Currently serving " + katzDeli[0]
   } else {
     return "There is nobody waiting to be served!"
   }
